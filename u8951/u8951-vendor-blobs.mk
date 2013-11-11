3e6cab96637d04bde35b77b2cf34f2180f874c8b
eloimuns <eloimuns@gmail.com>
2013-11-11 12:44:41 +0100

    Merge to U8951 and fix build

--- u8951/u8951-vendor-blobs.mk
+++ u8951/u8951-vendor-blobs.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/u8951/proprietary/bin/atserver:system/bin/atserver \
     vendor/huawei/u8951/proprietary/bin/bridgemgrd:system/bin/bridgemgrd \
     vendor/huawei/u8951/proprietary/bin/gpu_dcvsd:system/bin/gpu_dcvsd \
+    vendor/huawei/u8951/proprietary/bin/gps_check:system/bin/gps_check \
     vendor/huawei/u8951/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
     vendor/huawei/u8951/proprietary/bin/load_oemlogo:system/bin/load_oemlogo \
     vendor/huawei/u8951/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
